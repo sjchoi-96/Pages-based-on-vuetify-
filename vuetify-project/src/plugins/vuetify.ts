77710860ced120a6de581e7124f8e4e2b3f6df3c
sjchoi-96 <sj.choi@neobiotech.com>
2024-11-22 17:05:41 +0900

    vuetify.ts 수정

--- vuetify-project/src/plugins/vuetify.ts
+++ vuetify-project/src/plugins/vuetify.ts
@@ -1,18 +1,22 @@
-/**
- * plugins/vuetify.ts
- *
- * Framework documentation: https://vuetifyjs.com`
- */
-
 // Styles
 import "vuetify/styles";
 
 // Composables
 import { createVuetify } from "vuetify";
+import "vuetify/styles"; // Vuetify 기본 스타일
+import "@mdi/font/css/materialdesignicons.css"; // Material Design Icons
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: "dark",
+  },
+  icons: {
+    defaultSet: "mdi", // Material Design Icons를 기본 아이콘 세트로 설정
+    aliases, // 아이콘 별칭
+    sets: {
+      mdi, // MDI 아이콘 세트 추가
+    },
   },
 });
